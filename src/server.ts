8a733fc4d44e70f521a69f5be26a13016b3c0cd4
Eduardo <edu.amr@hotmail.com>
2024-11-07 11:35:09 -0300

    refactor: Adicionado CORS

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import path from "path";
 import express from "express";
-import { router } from "./routes";;
+import { router } from "./routes";
 import { connectToWhatsApp } from "./services/whatsapp";
 import errorMiddleware from "./middlewares/error";
-import 'express-async-errors';
+import "express-async-errors";
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const server = express()
     .use(express.json())
+    .use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "https://whatsjob.com.br");
+      res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Time-Zone");
+      res.header("Access-Control-Allow-Credentials", "true");
+      next();
+    })
     .use(router)
     .use(express.static(path.resolve(__dirname, "..", "public")))
     .use(errorMiddleware);
