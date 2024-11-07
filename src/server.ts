d29c3db5315984c9510ad2217f140ef6065158ac
Eduardo <edu.amr@hotmail.com>
2024-11-07 12:17:44 -0300

    refactor: Coloado google tag manager

--- src/server.ts
+++ src/server.ts
@@ -10,13 +10,6 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const server = express()
     .use(express.json())
-    .use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "https://whatsjob.com.br");
-      res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Time-Zone");
-      res.header("Access-Control-Allow-Credentials", "true");
-      next();
-    })
     .use(router)
     .use(express.static(path.resolve(__dirname, "..", "public")))
     .use(errorMiddleware);
